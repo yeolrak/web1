e41ee512455d329ff629b294bc478184c11abed4
yeolrak <58378490+yeolrak@users.noreply.github.com>
2020-10-02 22:21:01 +0530

    Update 8-1.php

--- 8-1.php
+++ 8-1.php
@@ -4,7 +4,7 @@
      <meta charset="utf-8">
 </head>
 <body>
-  <form action="8-2.php">
+  <form action="https://yeolrak.github.io/web1/8-2.php">
       <p>비밀번호를 입력해주세요.</p>
       <input type="text" name="password">
       <input type="submit">
